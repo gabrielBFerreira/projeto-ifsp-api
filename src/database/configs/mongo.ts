7b6a6f8ab1c4d1075b62d41f5e520bc26b4971d3
gabrielBFerreira <gabriel.brisolla.ferreira@gmail.com>
2022-06-21 21:54:12 -0300

    Atualizando conexão com bancos MySQL e Mongo

--- src/database/configs/mongo.ts
+++ src/database/configs/mongo.ts
@@ -5,7 +5,13 @@ const rootDir = process.env.NODE_ENV === 'dev' ? './src' : './dist';
 export const mongoConnection = new DataSource({
   name: 'mongodb',
   type: 'mongodb',
-  url: `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo/${process.env.MONGO_DATABASE}?authSource=admin`,
+  url: `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${
+    process.env.MONGO_INITDB_ROOT_PASSWORD
+  }@${
+    process.env.NODE_ENV === 'dev'
+      ? 'mongo'
+      : `127.0.0.1:${process.env.MONGO_PORT}`
+  }/${process.env.MONGO_DATABASE}?authSource=admin`,
   useUnifiedTopology: true,
   synchronize: false,
   logging: true,
